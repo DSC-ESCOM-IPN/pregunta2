e462651eb8466cd3b158a0005897f44a18703960
JoseRivera12 <josearmando1470@gmail.com>
2021-02-10 19:32:29 -0600

    ui&ux finished

---
+++ app/src/main/java/com/dscescom/pregunta2/MenuActivity.kt
@@ -0,0 +1,14 @@
+package com.dscescom.pregunta2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+    }
+
+}
